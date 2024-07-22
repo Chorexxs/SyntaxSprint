177d7f7091fe08ff01850bfbf3384e0f2e9958ee
Chorexxs <oswaldofonseca159@gmail.com>
2024-07-22 23:31:25 +0200

    trabajando la lógica del registro del usuario

--- syntax_sprint/backend/views.py
+++ syntax_sprint/backend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
@@ -9,21 +10,37 @@ def index(request):
     return render(request, "index.html")
 
 
-def singup(request):
+def signup(request):
     if request.method == "POST":
         email = request.POST["email"]
-        name = request.POST["name"]
+        username = request.POST["username"]
         password = request.POST["password"]
         password2 = request.POST["password2"]
 
         if password == password2:
-            pass
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Ya existe este email")
+                return redirect("signup")
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, "El nombre de usuario ya existe")
+                return redirect(signup)
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+
+                # Autentificando al usuario
+                user_login = auth.authenticate(
+                    username=username, password=password)
+                auth.login(request, user_login)
+                return redirect("/")
+
         else:
             messages.info(request, "La contraseña no coincide")
-            return redirect("singup")
+            return redirect("signup")
 
     else:
-        return render(request, "singup.html")
+        return render(request, "signup.html")
 
 
 def login(request):
