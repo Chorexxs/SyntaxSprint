fd6fc8fcb29c9707912b4a7a07f532e92bf81b04
Chorexxs <oswaldofonseca159@gmail.com>
2024-08-26 23:00:52 +0200

    Update manage.py

--- syntax_sprint/manage.py
+++ syntax_sprint/manage.py
@@ -6,8 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'syntax_sprint.syntax_sprint.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syntax_sprint.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
