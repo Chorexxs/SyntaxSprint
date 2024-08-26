c4bc1a67b917498657c74303783aac8e0a17dfdb
Chorexxs <oswaldofonseca159@gmail.com>
2024-08-26 21:22:41 +0200

    editando rutas para el deploy

--- syntax_sprint/syntax_sprint/wsgi.py
+++ syntax_sprint/syntax_sprint/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'syntax_sprint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                      'syntax_sprint.syntax_sprint.settings')
 
 application = get_wsgi_application()
